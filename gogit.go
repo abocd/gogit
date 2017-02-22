e3e9a0317654b9aab923e44a71150d2801af2055
aboc <abocd@qq.com>
2017-02-22 09:11:24 +0800

    添加目录、git库判断

--- gogit.go
+++ gogit.go
@@ -4,13 +4,15 @@ import (
 	//"fmt"
 	"flag"
 	"github.com/abocd/gogit/glog"
-	"log"
+	//"log"
 	"strconv"
 	"net/http"
 	"path"
+	"os"
+	//"fmt"
 )
 
-var gitrepo string;
+var dirpath string;
 
 func www(w http.ResponseWriter, r *http.Request) {
 
@@ -20,17 +22,30 @@ func main(){
 	//前台访问端口
 	port := flag.Int("p",7878,"Port");
 	//git地址
-	gitrepo = flag.String("r","","Git path");
+	gitrepo := flag.String("r","","Git path");
 	flag.Parse();
+	dirpath = *gitrepo;
 	//fmt.Printf("%s %d",*path,*port);
-	glog.Info("Start","Git目录:%s,浏览器访问 IP:%d",*gitrepo,*port)
-
-	if(path.)
-
-	http.HandleFunc("/", www)
-	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
+	fileInfo,err :=os.Stat(dirpath);
+	if err != nil{
+		glog.Error("Break","%s %s",dirpath,err);
+		return;
 	}
-
+	//fmt.Println(fileInfo);
+	if !fileInfo.IsDir(){
+		glog.Error("Break","%s 不是一个目录",dirpath);
+		return;
+	}
+	_,err2 := os.Stat(path.Clean(dirpath+"/.git"));
+	if err2 != nil{
+		glog.Error("Break","%s 不是一个有效的git版本库",dirpath);
+		return;
+	}
+	http.HandleFunc("/", www)
+	err = http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+	if err != nil {
+		glog.Error("Bad","ListenAndServe:%s", err);
+		return;
+	}
+	glog.Info("Start","Git目录:%s,浏览器访问 IP:%d",dirpath,*port)
 }
