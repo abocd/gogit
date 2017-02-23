635a24b06c383ef16c1c96638b0387adf4437478
aboc <abocd@qq.com>
2017-02-23 13:52:20 +0800

    cmd

--- gogit.go
+++ gogit.go
@@ -9,13 +9,27 @@ import (
 	"net/http"
 	"path"
 	"os"
-	//"fmt"
+	"os/exec"
+	//"bytes"
+	"strings"
+	"fmt"
 )
 
 var dirpath string;
 
 func www(w http.ResponseWriter, r *http.Request) {
+	//f,err := exec.Command("cd",dirpath).Output();
+	result,_ := exec.LookPath(dirpath);
+	glog.Info("Log",result);
 
+	f,err := exec.Command("git","log").Output();
+	if err != nil{
+		glog.Error("Cmd Error",err.Error());
+		return;
+	}
+	data := strings.Split(string(f),"\n");
+	fmt.Println(data);
+	glog.Info("Cmd","%d (%s)",len(data),data[0]);
 }
 
 func main(){
@@ -41,11 +55,11 @@ func main(){
 		glog.Error("Break","%s 不是一个有效的git版本库",dirpath);
 		return;
 	}
+	glog.Info("Start","Git目录:%s,浏览器访问 IP:%d",dirpath,*port)
 	http.HandleFunc("/", www)
 	err = http.ListenAndServe(":"+strconv.Itoa(*port), nil)
 	if err != nil {
 		glog.Error("Bad","ListenAndServe:%s", err);
 		return;
 	}
-	glog.Info("Start","Git目录:%s,浏览器访问 IP:%d",dirpath,*port)
 }
